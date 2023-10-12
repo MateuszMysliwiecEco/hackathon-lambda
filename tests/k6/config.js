8b5fe48b5f55b7a04138476ac69a38c449f2740d
MateuszMysliwiecEco <mateusz.mysliwiec@ecoonline.com>
2023-10-12 22:26:37 +0100

    Remove unused files

--- tests/k6/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    version: '1.0.0',
-    url: 'https://auth.test.ecoonline.net',
-    company_code: 'ecodemo',
-    username: 'Pavan',
-    password: 'Pav1234!',
-};
-   
-module.exports = config;
